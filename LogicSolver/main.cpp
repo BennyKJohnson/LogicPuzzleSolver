f837a05d1f70b56c80e11f47362fb129aa3bf471
BennyKJohnson <benjohnson@The-Internet.local>
2014-06-14 15:15:31 +1000

    Initial Commit

---
+++ LogicSolver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LogicSolver
+//
+//  Created by Ben Johnson on 14/06/2014.
+//  Copyright (c) 2014 Ben Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
